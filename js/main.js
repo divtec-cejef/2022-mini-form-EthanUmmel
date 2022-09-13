7bb291ad32b72a3570030b7ffb03052686bb964f
EthanUmmel <94692709+EthanUmmel@users.noreply.github.com>
2022-09-13 16:30:10 +0200

    Exercice html et css finis. Manque le java

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 /**
- * @author Steve Fallet
- * @since 2022.09.06
+ * @author Ethan Ummel
+ * @since 2022.09.13
  */
 
 'use strict';
+const formulaire = document.querySelector('form');
+const elementliste = document.getElementsByTagName('background')
+
+
+formulaire.addEventListener('change', function(event) {
+
+});
 
 console.log('Test liaison JS');
